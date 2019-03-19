623a5408c453ec441fd811445ffc2714fea27e9a
Jason Burns <jasonb315@protonmail.com>
2019-03-19 10:52:19 -0700

    the right way, or again

--- Lab01-numbers-game/Program.cs
+++ Lab01-numbers-game/Program.cs
@@ -28,50 +28,59 @@ namespace Lab01_numbers_game
 
         static void StartSequence()
         {
-            Console.WriteLine("Enter a number greater than zero");
-            string inputStr = Console.ReadLine();
-            int inputVal;
-            inputVal = Convert.ToInt32(inputStr);
-            int[] intArr = new int[inputVal];
-            Populate(intArr);
+            //Console.WriteLine("Enter a number greater than zero");
+            //string inputStr = Console.ReadLine();
+            //int inputVal;
+            //inputVal = Convert.ToInt32(inputStr);
+
+            //int[] intArr = new int[inputVal];
+
+            //int[] population = Populate(intArr);
+            //int sum = GetSum(population);
         }
 
-        static void Populate(int[] intArr)
+        static int[] Populate(int[] intArr)
         {
 
-            for (int i = 0 ; i < intArr.Length ; i ++){
-                Console.Write("Please enter number {0} of {1}:", i + 1, intArr.Length);
-                string inputStr = Console.ReadLine();
-                int inputVal;
-                inputVal = Convert.ToInt32(inputStr);
-                intArr[i] = inputVal;
+            //for (int i = 0 ; i < intArr.Length ; i ++){
+            //    Console.Write("Please enter number {0} of {1}:", i + 1, intArr.Length);
+            //    string inputStr = Console.ReadLine();
+            //    int inputVal;
+            //    inputVal = Convert.ToInt32(inputStr);
+            //    intArr[i] = inputVal;
 
-                GetSum(intArr);
-            }
+            //    return intArr;
+            //}
 
         }
         static void GetSum(int[] intArr)
         {
-            int sum = 0;
-            for (int i = 0; i < intArr.Length; i++)
-            {
-                sum += intArr[i];
-            }
-            GetProduct(intArr, sum);
+            //int sum = 0;
+            //for (int i = 0; i < intArr.Length; i++)
+            //{
+            //    sum += intArr[i];
+            //}
+            //GetProduct(intArr, sum);
 
         static void GetProduct(int[] intArr, int sum)
         {
-                Console.WriteLine("Please select a random number between 1 and {0}", intArr.Length);
-                string inputStr = Console.ReadLine();
-                int inputVal;
-                inputVal = Convert.ToInt32(inputStr);
-                int product = intArr[inputVal] * sum;
+            //Console.WriteLine("Please select a random number between 1 and {0}", intArr.Length);
+            //string inputStr = Console.ReadLine();
+            //int inputVal;
+            //inputVal = Convert.ToInt32(inputStr);
+            //int product = intArr[inputVal] * sum;
 
-            }
+            //GetQuotient(product);
+        }
 
-        static void GetQuotient()
+        static void GetQuotient(int product)
         {
+            //Console.WriteLine("Please enter a number to divide your product {0} by", product);
+            //string inputStr = Console.ReadLine();
+            //int inputVal;
+            //inputVal = Convert.ToInt32(inputStr);
+
+            
 
         }
-    }
 }

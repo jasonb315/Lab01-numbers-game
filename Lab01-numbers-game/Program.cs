e67327933ecc48e280eb5e800b90253d826653f1
Jason Burns <jasonb315@protonmail.com>
2019-03-19 10:23:06 -0700

    Main

--- Lab01-numbers-game/Program.cs
+++ Lab01-numbers-game/Program.cs
@@ -6,8 +6,49 @@ namespace Lab01_numbers_game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
-            Console.WriteLine("Hello World!!!");
+            string appName = "Numbers Game";
+            string appVersion = "1.0.0";
+            string appAuthor = "Jason Burns";
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            try
+            {
+                StartSequence();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+            finally
+            {
+                Console.Write("Program complete.");
+            }
+
+        }
+
+        static void StartSequence()
+        {
+
+        }
+
+        static void Populate(int[] intArr)
+        {
+
+
+
+        }
+        static void GetSum()
+        {
+
+        }
+
+        static void GetProduct()
+        {
+
+        }
+
+        static void GetQuotient()
+        {
 
         }
     }
